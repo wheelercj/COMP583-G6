7ced962bd693c0f2ead42e70c456a7e68f2efc70
jwlim97 <monbongo97@gmail.com>
2023-03-31 17:47:20 -0700

    JS,HTML,CSS for about and plan upgrading page

---
+++ button.js
@@ -0,0 +1,14 @@
+/*
+Programmer Name: Jong Won Lim
+*/
+const aboutBtn = document.getElementById("about-btn");
+
+aboutBtn.addEventListener("click", function() {
+  window.location.href = "about.html";
+});
+
+const upgradeBtn = document.getElementById("upgrade-btn");
+
+upgradeBtn.addEventListener("click", function() {
+  window.location.href = "upgrade.html";
+});
