f9cebd495687f0cdcf2ed26adad2ce900ab8dfb5
wheelercj <chrisw289@gmail.com>
2023-04-18 11:45:41 -0700

    Simplify server.js

--- build/server.js
+++ build/server.js
@@ -7,7 +7,7 @@ const fs = require("fs").promises;
 const host = "localhost";
 const port = 8000;
 
-const requestListener = function (req, res) {
+const server = http.createServer(function (req, res) {
     fs.readFile(__dirname + "/index.html")
         .then(contents => {
             res.setHeader("Content-Type", "text/html");
@@ -19,9 +19,8 @@ const requestListener = function (req, res) {
             res.end(err);
             return;
         });
-};
+});
 
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+server.listen(port, host, function () {
+    console.log(`Server running at http://${host}:${port}`);
 });
