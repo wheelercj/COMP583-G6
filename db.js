c983dffdc365e7d9f1818ef1c454c576b503b7cf
wheelercj <chrisw289@gmail.com>
2023-04-25 15:32:07 -0700

    Use more env vars

--- db.js
+++ db.js
@@ -9,11 +9,11 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: process.env['DB_HOST'],
-    port: '3306',
-    database: 'url_shortener',
-    user: 'admin',
+    port: process.env['DB_PORT'],
+    database: process.env['DB_NAME'],
+    user: process.env['DB_USER'],
     password: process.env['DB_PASSWORD'],
-    ssl: 'Amazon RDS',
+    ssl: process.env['DB_SSL'],
 });
 
 
